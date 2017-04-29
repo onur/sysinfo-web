b0ff2cf6dcaf334c78ba5af83815a5faa341d188
Guillaume Gomez <guillaume1.gomez@gmail.com>
2017-04-29 12:09:37 +0200

    If no user connected in 10 minutes, system information won't be updated

--- src/web.rs
+++ src/web.rs
@@ -7,17 +7,34 @@ use iron::middleware::Handler;
 use iron::mime::Mime;
 
 use sysinfo::{System, SystemExt};
-use std::sync::{Arc, RwLock};
+use std::sync::{Arc, Mutex, RwLock};
 use std::thread;
-use std::time::Duration;
+use std::time::{Duration, SystemTime};
 use SysinfoExt;
 use serde_json;
 
 
 const INDEX_HTML: &'static str = include_str!("index.html");
+const REFRESH_DELAY: u64 = 60 * 10; // 10 minutes
 
-struct SysinfoIronHandler(Arc<RwLock<System>>);
+struct SysinfoIronHandler(Arc<DataHandler>);
 
+struct DataHandler {
+    system: RwLock<System>,
+    last_connection: Mutex<SystemTime>,
+}
+
+impl DataHandler {
+    fn can_update_system_info(&self) -> bool {
+        SystemTime::now().duration_since(*self.last_connection.lock().unwrap())
+                         .unwrap()
+                         .as_secs() < REFRESH_DELAY
+    }
+
+    fn update_last_connection(&self) {
+        *self.last_connection.lock().unwrap() = SystemTime::now();
+    }
+}
 
 impl Handler for SysinfoIronHandler {
     fn handle(&self, req: &mut Request) -> IronResult<Response> {
@@ -28,7 +45,8 @@ impl Handler for SysinfoIronHandler {
                                        "text/html".parse::<Mime>().unwrap(),
                                        INDEX_HTML)))
                 } else {
-                    let system = self.0.read().unwrap();
+                    self.0.update_last_connection();
+                    let system = self.0.system.read().unwrap();
                     let sysinfo = SysinfoExt::new(&system);
                     Ok(Response::with((status::Ok,
                                        "application/json".parse::<Mime>().unwrap(),
@@ -42,18 +60,26 @@ impl Handler for SysinfoIronHandler {
 
 
 pub fn start_web_server(sock_addr: Option<String>) -> HttpResult<Listening> {
-    let system = Arc::new(RwLock::new(System::new()));
-    let system_clone = system.clone();
+    let data_handler = Arc::new(DataHandler {
+        system: RwLock::new(System::new()),
+        last_connection: Mutex::new(SystemTime::now()),
+    });
+    let data_handler_clone = data_handler.clone();
     thread::spawn(move || {
         loop {
-            {
-                let mut system = system_clone.write().unwrap();
-                system.refresh_all();
+            if data_handler_clone.can_update_system_info() {
+                {
+                    let mut system = data_handler_clone.system.write().unwrap();
+                    system.refresh_all();
+                }
+                thread::sleep(Duration::new(5, 0));
+            } else {
+                // If we don't need to refresh the system information, we can sleep a lot less.
+                thread::sleep(Duration::from_millis(500));
             }
-            thread::sleep(Duration::new(5, 0));
         }
     });
-    let mut iron = Iron::new(SysinfoIronHandler(system));
+    let mut iron = Iron::new(SysinfoIronHandler(data_handler));
     iron.threads = 4;
     iron.http(sock_addr.unwrap_or("localhost:3000".to_owned()))
 }
