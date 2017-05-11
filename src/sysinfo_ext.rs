a0a93df473dc5ff9f20a89399fae85e9f878c6f4
Onur Aslan <onuraslan@gmail.com>
2017-05-11 20:33:17 +0300

    Add network usage

--- src/sysinfo_ext.rs
+++ src/sysinfo_ext.rs
@@ -1,6 +1,8 @@
 
 use std::collections::HashMap;
-use std::io::Error;
+use std::io::{Error, ErrorKind, Read};
+use std::fs::File;
+use std::{thread, time};
 use serde::{Serialize, Serializer};
 use sysinfo::{System, SystemExt, Disk, Process, Component, Processor};
 use hostname::get_hostname;
@@ -15,6 +17,7 @@ pub struct SysinfoExt<'a> {
     memory: [u64; 6],
     hostname: String,
     uptime: String,
+    bandwith: (u64, u64),
 }
 
 
@@ -51,6 +54,7 @@ impl<'a> SysinfoExt<'a> {
                      system.get_free_swap()],
             hostname: get_hostname().unwrap_or("Unknown".to_owned()),
             uptime: get_uptime().unwrap_or("Unknown".to_owned()),
+            bandwith: get_bandwith_usage().unwrap_or((0, 0)),
         }
     }
 }
@@ -83,6 +87,9 @@ impl<'a> Serialize for SysinfoExt<'a> {
         map.serialize_entry("memory", &self.memory)?;
         map.serialize_entry("hostname", &self.hostname)?;
         map.serialize_entry("uptime", &self.uptime)?;
+        map.serialize_entry("bandwith", &self.bandwith)?;
+        #[cfg(target_os = "linux")]
+        map.serialize_entry("show_bandwith_usage", &true)?;
         map.end()
     }
 }
@@ -93,4 +100,44 @@ fn get_uptime() -> Result<String, Error> {
     use std::process::Command;
     let output = Command::new("uptime").output()?;
     Ok(String::from_utf8(output.stdout).unwrap_or("Unknown".to_owned()))
+}
+
+
+#[cfg(target_os = "linux")]
+/// Gets bandwith usage
+///
+/// Returns (Incoming, Outgoing) bytes per seconds
+fn get_bandwith_usage() -> Result<(u64, u64), Error> {
+    fn read_interface_stat(iface: &str, typ: &str) -> Result<u64, Error> {
+        let mut file = File::open(format!("/sys/class/net/{}/statistics/{}_bytes", iface, typ))?;
+        let mut content = String::new();
+        file.read_to_string(&mut content)?;
+        content.trim().parse()
+            .map_err(|_| Error::new(ErrorKind::Other, "Failed to parse network stat"))
+    }
+
+    let default_interface = {
+        let mut file = File::open("/proc/net/route")?;
+        let mut content = String::new();
+        file.read_to_string(&mut content)?;
+        content.lines()
+            .filter(|l| l.split_whitespace().nth(2).map(|l| l != "00000000").unwrap_or(false))
+            .last()
+            .and_then(|l| l.split_whitespace().nth(0))
+            .ok_or(Error::new(ErrorKind::Other, "Default device not found"))?
+            .to_owned()
+    };
+
+    let (old_rx, old_tx) = (read_interface_stat(&default_interface, "rx")?,
+                            read_interface_stat(&default_interface, "tx")?);
+    thread::sleep(time::Duration::from_millis(500));
+    let (new_rx, new_tx) = (read_interface_stat(&default_interface, "rx")?,
+                            read_interface_stat(&default_interface, "tx")?);
+
+    Ok(((new_rx - old_rx) * 2, (new_tx - old_tx) * 2))
+}
+
+#[cfg(not(target_os = "linux"))]
+fn get_bandwith_usage() -> Result<(u64, u64), Error> {
+    Ok((0, 0))
 }
